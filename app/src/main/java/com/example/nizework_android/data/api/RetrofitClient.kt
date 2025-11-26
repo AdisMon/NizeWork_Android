614356c307c34c8fc885e382f7a3e15d70baef96
Adis <212310066@itslerdo.edu.mx>
2025-11-25 20:07:54 -0600

    ligero

--- app/src/main/java/com/example/nizework_android/data/api/RetrofitClient.kt
+++ app/src/main/java/com/example/nizework_android/data/api/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://169.254.211.179:5000/"
+    private const val BASE_URL = "http://192.168.137.201:5000/"
 
     val instance: Retrofit by lazy {
         Retrofit.Builder()
